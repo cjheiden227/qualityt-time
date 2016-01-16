548447fd5568a80a2c14674174deb6abc1dc9ce8
Chris Heiden <cjheiden227@gmail.com>
2016-01-16 15:29:53 -0800

    Added blog posts to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@latest_episode =  Episode.order("published_at").last
+		@latest_posts =  Monologue::Post.order("published_at DESC").limit(6)
 	end
 end
