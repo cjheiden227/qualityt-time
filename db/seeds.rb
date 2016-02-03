0325fcef41938bf746f4b14f18f50d5f288dfdb5
Chris Heiden <cjheiden227@gmail.com>
2016-02-02 20:39:33 -0800

    added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Monologue::User.create(name:"Chris", email: "cjheiden227@gmail.com", password:"swes3ub5", password_confirmation:"swes3ub5")
+Episode.episodes_task
